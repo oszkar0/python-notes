1697468f668e35fbda2d706644419cefece2e3c6
oszkar0 <oskszysiak@gmail.com>
2023-07-22 19:27:40 +0200

    Module 8: Pylint and Unit Testing with unittest package

---
+++ Module 8 Pylint and Unit testing/Pylint/simple1.py
@@ -0,0 +1,7 @@
+#python -m pylint simple1.py in cmd
+#shows messanges about errors and 
+
+a = 1
+b = 2
+print(a)
+print(B)
---
+++ Module 8 Pylint and Unit testing/Pylint/simple1_beautified.py
@@ -0,0 +1,16 @@
+#python -m pylint simple1.py in cmd
+#shows messanges about errors and 
+'''
+A Very Simple Script
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
